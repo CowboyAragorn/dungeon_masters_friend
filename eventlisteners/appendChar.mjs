885cf58f05fd716002bf9de21cb503802dcb658f
CowboyAragorn <jacobman0313@gmail.com>
2023-02-27 13:25:33 -0500

    redesign: change display of character features into a grid format over sentences html skeleton

--- eventlisteners/appendChar.mjs
+++ eventlisteners/appendChar.mjs
@@ -2,65 +2,55 @@ import { Character, raceClasses } from "../characterObjects.mjs";
 import getRandomIntInclusive from "../utils/getRandomIntInclusive.mjs";
 
 function appendChar() {
-  const charContainer = document.getElementById("charContainer");
+  function selectRandChar() {
+    const arrOfRaces = Object.keys(raceClasses);
+    const randNum = getRandomIntInclusive(0, arrOfRaces.length - 1);
+    const randRace = arrOfRaces[randNum];
+    return raceClasses[randRace];
+  }
+
+  function clearContainer(container) {
+    while (container.firstChild) {
+      container.firstChild.remove();
+    }
+  }
+
+  function appendListToContainer(container, items) {
+    for (let i = 0; i < items.length; i++) {
+      let itemToAppend = document.createElement("p");
+      itemToAppend.innerHTML = items[i];
+      container.append(itemToAppend);
+    }
+  }
+  function appendToPage(char) {
+    const nameElement = document.getElementById("name");
+    nameElement.innerHTML = char.firstName + " " + char.lastName;
+
+    const raceElement = document.getElementById("race");
+    raceElement.innerHTML = char.race;
+    const genderElement = document.getElementById("gender");
+    genderElement.innerHTML = char.gender;
+    const ageElement = document.getElementById("age");
+    ageElement.innerHTML = char.age + " years old";
+
+    const occupationsContainer = document.getElementById(
+      "occupationsContainer"
+    );
+    clearContainer(occupationsContainer);
+    appendListToContainer(occupationsContainer, char.job);
+
+    const traitsContainer = document.getElementById("traitsContainer");
+    clearContainer(traitsContainer);
+    appendListToContainer(traitsContainer, char.traits);
+
+    const desiresElement = document.getElementById("desires");
+    desiresElement.innerHTML = char.desires;
+  }
+
   const randRace = selectRandChar();
   const char = new randRace();
+  appendToPage(char);
   console.log(char);
-  //rest the list to show new char if button is clicked again
-  while (charContainer.firstChild) {
-    charContainer.firstChild.remove();
-  }
-  const nameLine = document.createElement("p");
-  nameLine.innerHTML =
-    char.firstName +
-    " " +
-    char.lastName +
-    " is a " +
-    char.age +
-    " year old " +
-    char.gender +
-    " " +
-    char.race;
-  charContainer.append(nameLine);
-
-  const jobString = createStrFromList(char, "job");
-  const jobLine = document.createElement("p");
-  jobLine.innerHTML = char.pronoun.subject + " is a " + jobString;
-  charContainer.append(jobLine);
-
-  const traitsString = createStrFromList(char, "traits");
-  const traitsLine = document.createElement("p");
-  traitsLine.innerHTML = char.pronoun.subject + " is " + traitsString;
-  charContainer.append(traitsLine);
-
-  const desireLine = document.createElement("P");
-  desireLine.innerHTML = char.pronoun.subject + " desires " + char.desires;
-  charContainer.append(desireLine);
-}
-
-//createStrFromList takes in the character and an array to generate a string
-//of that array for format "is a x, y, and z"
-function createStrFromList(char, list) {
-  let newStr = "";
-  for (let i = 0; i < char[list].length; i++) {
-    if (char[list].length == 1) {
-      newStr = char[list][i] + ".";
-      break;
-    }
-    if (i == char[list].length - 1) {
-      newStr += "and " + char[list][i] + ".";
-      break;
-    }
-    newStr += char[list][i] + ", ";
-  }
-  return newStr;
-}
-
-function selectRandChar() {
-  const arrOfRaces = Object.keys(raceClasses);
-  const randNum = getRandomIntInclusive(0, arrOfRaces.length - 1);
-  const randRace = arrOfRaces[randNum];
-  return raceClasses[randRace];
 }
 
 export default appendChar;

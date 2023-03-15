6ba403e41a47e570ed31d6155502538612e94db4
CowboyAragorn <jacobman0313@gmail.com>
2023-03-15 17:56:56 -0400

    feat: add numbering to queue dom display

--- eventlisteners/appendCharQueue.mjs
+++ eventlisteners/appendCharQueue.mjs
@@ -5,6 +5,7 @@ export default function appendCharStack() {
   let j = 0;
   for (let i = charQueue.length; i > 0; i--) {
     const queueContainerChildren = queueContainer.childNodes;
+    console.log(queueContainerChildren);
     const innerHTMLIndentifier =
       charQueue[i - 1].firstName +
       " " +
@@ -13,15 +14,35 @@ export default function appendCharStack() {
       charQueue[i - 1].race;
     //if the node exists, and if its name is the same, no need to append again
     if (queueContainerChildren[j]) {
-      if (queueContainerChildren[j].innerHTML == innerHTMLIndentifier) {
+      const queueContainerCurrentChild = queueContainerChildren[j].children[1];
+      if (queueContainerCurrentChild.innerHTML == innerHTMLIndentifier) {
         j++;
         continue;
       }
     }
     j++;
+
+    const queueBox = document.createElement("div");
+    queueBox.classList.add("queueBox");
+    queueContainer.prepend(queueBox);
+
+    const numberDisplay = document.createElement("p");
+    numberDisplay.classList.add("queueNumber");
+    queueBox.append(numberDisplay);
+
     const charBtn = document.createElement("button");
     charBtn.classList.add("charBtn");
     charBtn.innerHTML = innerHTMLIndentifier;
-    queueContainer.prepend(charBtn);
+    queueBox.append(charBtn);
+    numberCharQueue();
+  }
+}
+
+function numberCharQueue() {
+  const queueContainer = document.getElementById("queueContainer");
+  const queueContainerChildren = queueContainer.childNodes;
+  for (let i = 0; i < charQueue.length; i++) {
+    const queueContainerCurrentChild = queueContainerChildren[i].children[0];
+    queueContainerCurrentChild.innerHTML = i + 1;
   }
 }
